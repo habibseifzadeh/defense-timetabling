bdc4df5cd435cd5db343973fa85a5a340637aa41
Habib Seifzadeh <habibseifzadeh@gmail.com>
2019-01-16 10:25:52 +0330

    An empty Spring application with the required packages created by Spring Initializr

---
+++ src/main/java/com/seif/defensetimetabling/DefenseTimetablingApplication.java
@@ -0,0 +1,14 @@
+package com.seif.defensetimetabling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefenseTimetablingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefenseTimetablingApplication.class, args);
+    }
+
+}
+
