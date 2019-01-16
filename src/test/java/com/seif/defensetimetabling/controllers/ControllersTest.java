7353f0478c76bd20ad336151f31034c5a081f161
Habib Seifzadeh <habibseifzadeh@gmail.com>
2019-01-17 00:49:21 +0330

    Bug fixing in tests: since defenseSession Service had been added to the index controller, I had to add a mock of this service to the test case of index controller

--- src/test/java/com/seif/defensetimetabling/controllers/ControllersTest.java
+++ src/test/java/com/seif/defensetimetabling/controllers/ControllersTest.java
@@ -1,17 +1,24 @@
 package com.seif.defensetimetabling.controllers;
 
+import com.seif.defensetimetabling.services.DefenseSessionService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class ControllersTest {
+
+    @Mock
+    private DefenseSessionService defenseSessionService;
 
     @InjectMocks
     private IndexController indexController;
@@ -30,5 +37,7 @@ public class ControllersTest {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"));
+
+        verify(defenseSessionService, times(1)).findAll();
     }
 }
